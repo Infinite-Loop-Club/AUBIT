271c93d5af8f50d4f42c86347a898660ed030dbb
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-23 17:15:10 +0530

    improved admin cards

--- app/src/components/Card/Adminstration/Card.jsx
+++ app/src/components/Card/Adminstration/Card.jsx
@@ -5,12 +5,14 @@ export default (props) => {
   return (
     <div className={styles.card}>
       <div className={styles.img}>
-        <img src={props.image} alt={"vice chancellor"} />
+        <img src={props.image} alt={props.designation} />
       </div>
       <div className={styles.details}>
-        <h3 className={styles.heading_primary_main}>{props.name}</h3>
-        <p className={styles.description}>{props.designation}</p>
-        <h6>Intercom : {props.inter}</h6>
+        <div className={styles.basic}>
+          <h3 className={styles.heading_primary_main}>{props.name}</h3>
+          <p className={styles.designation}>{props.designation}</p>
+        </div>
+        <p className={styles.inter}>Intercom : {props.inter}</p>
         <div className={styles.details__phn}>
           <span className="icon-phone" style={{ color: "#a61d22" }}></span>
           <p>{props.phn}</p>
