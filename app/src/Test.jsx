2f0b1ffa6dd5863694e26f07a39dbef2666bbde0
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-24 21:02:54 +0530

    🌳 added card modularity

--- app/src/Test.jsx
+++ app/src/Test.jsx
@@ -1,4 +1,11 @@
 import React from "react";
 import StaffCard from "./components/Card/Staff";
 
-export default () => <div style={{ display: "grid", height: '100vh', placeItems: 'center' }}> <StaffCard /> </div>;
+export default () => <div style={{ display: "grid", height: '100vh', placeItems: 'center' }}>
+  <StaffCard
+    image='./images/administration/vc.png'
+    name='Sandy'
+    designation='Sandy pola varuma'
+    resume='#'
+    mail='santhoshvelr@gmail.com' />
+</div>;
--- app/src/components/Card/Staff/StaffCard.jsx
+++ app/src/components/Card/Staff/StaffCard.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import classes from './StaffCard.module.scss';
 
-export default function StaffCard() {
+export default function StaffCard({ style, image, name, designation, mail, resume }) {
   return (
     <div className={classes.card}>
       <div className={classes.card__frame}>
         <div className={classes.card__figure}>
-          <img className={classes.card__image} src='./images/administration/vc.png' />
+          <img className={classes.card__image} src={image} />
         </div>
         <div className={classes.card__details}>
-          <h3 className={classes.card__details_name}>Santhosh</h3>
-          <p className={classes.card__details_designation}>Sandy pola varuma?</p>
+          <h3 className={classes.card__details_name}>{name}</h3>
+          <p className={classes.card__details_designation}>{designation}</p>
         </div>
       </div>
 
       <div className={classes.card__cta}>
-        <a href='#link' className={classes.card__link}>
+        <a href={`mailto: ${mail}`} className={classes.card__link}>
           <span className='icon-mail' />
         </a>
-        <a href='#link' className={classes.card__link}>
+        <a href={resume} className={classes.card__link}>
           <span className='icon-paperclip' /></a>
       </div>
     </div>
