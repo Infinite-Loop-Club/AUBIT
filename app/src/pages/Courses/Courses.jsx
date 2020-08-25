009558c2fc64ba601c832f69a97509d5c19ae038
Pranav <pranav.gnanasekar23@gmail.com>
2020-08-25 20:46:48 +0530

    courses page table add

--- app/src/pages/Courses/Courses.jsx
+++ app/src/pages/Courses/Courses.jsx
@@ -2,8 +2,9 @@ import React, { Fragment } from "react";
 import styles from "./style.module.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Table from "../../components/Table/Table";
 
-function Courses() {
+export default function Courses() {
   return (
     <Fragment>
       <Header sticky />
@@ -14,10 +15,135 @@ function Courses() {
         >
           Department of Automobile Engineering
         </h4>
+        <Table data={data} />
       </main>
       <Footer />
     </Fragment>
   );
 }
 
-export default Courses;
+const data = {
+  header: ["Name", "Contact Address", "BoG", "Mail ID", "Contact No"],
+  values: [
+    [
+      <p>
+        Mr.R.Balasubramania
+        <br /> Former Member,
+        <br /> Tamilnadu Electricity Regulatory Commission,
+        <br /> Chennai, & Chief Engineer,
+        <br /> TNEB, Chennai
+      </p>,
+      <p>
+        Former Member Secretary, TERC,
+        <br />
+        258 / 6, Swathi Appartments,
+        <br />
+        Opp.to Tirumangalam Police Station
+        <br /> Chennai - 600 101.
+      </p>,
+      "Chairman",
+      "seethalakshmib@hotmail.com",
+      "9840055645",
+    ],
+    [
+      <p>
+        Mr.R.Balasubramania
+        <br /> Former Member,
+        <br /> Tamilnadu Electricity Regulatory Commission,
+        <br /> Chennai, & Chief Engineer,
+        <br /> TNEB, Chennai
+      </p>,
+      <p>
+        Former Member Secretary, TERC,
+        <br />
+        258 / 6, Swathi Appartments,
+        <br />
+        Opp.to Tirumangalam Police Station
+        <br /> Chennai - 600 101.
+      </p>,
+      "Chairman",
+      "seethalakshmib@hotmail.com",
+      "9840055645",
+    ],
+    [
+      <p>
+        Mr.R.Balasubramania
+        <br /> Former Member,
+        <br /> Tamilnadu Electricity Regulatory Commission,
+        <br /> Chennai, & Chief Engineer,
+        <br /> TNEB, Chennai
+      </p>,
+      <p>
+        Former Member Secretary, TERC,
+        <br />
+        258 / 6, Swathi Appartments,
+        <br />
+        Opp.to Tirumangalam Police Station
+        <br /> Chennai - 600 101.
+      </p>,
+      "Chairman",
+      "seethalakshmib@hotmail.com",
+      "9840055645",
+    ],
+    [
+      <p>
+        Mr.R.Balasubramania
+        <br /> Former Member,
+        <br /> Tamilnadu Electricity Regulatory Commission,
+        <br /> Chennai, & Chief Engineer,
+        <br /> TNEB, Chennai
+      </p>,
+      <p>
+        Former Member Secretary, TERC,
+        <br />
+        258 / 6, Swathi Appartments,
+        <br />
+        Opp.to Tirumangalam Police Station
+        <br /> Chennai - 600 101.
+      </p>,
+      "Chairman",
+      "seethalakshmib@hotmail.com",
+      "9840055645",
+    ],
+    [
+      <p>
+        Mr.R.Balasubramania
+        <br /> Former Member,
+        <br /> Tamilnadu Electricity Regulatory Commission,
+        <br /> Chennai, & Chief Engineer,
+        <br /> TNEB, Chennai
+      </p>,
+      <p>
+        Former Member Secretary, TERC,
+        <br />
+        258 / 6, Swathi Appartments,
+        <br />
+        Opp.to Tirumangalam Police Station
+        <br /> Chennai - 600 101.
+      </p>,
+      "Chairman",
+      "seethalakshmib@hotmail.com",
+      "9840055645",
+    ],
+    [
+      <p>
+        Mr.R.Balasubramania
+        <br /> Former Member,
+        <br /> Tamilnadu Electricity Regulatory Commission,
+        <br /> Chennai, & Chief Engineer,
+        <br /> TNEB, Chennai
+      </p>,
+      <p>
+        Former Member Secretary, TERC,
+        <br />
+        258 / 6, Swathi Appartments,
+        <br />
+        Opp.to Tirumangalam Police Station
+        <br /> Chennai - 600 101.
+      </p>,
+      "Chairman",
+      "seethalakshmib@hotmail.com",
+      "9840055645",
+    ],
+  ],
+};
