b566c1b608596822039a77bc450062ffce305fab
sank2000 <santhoshvelr@yahoo.com>
2020-08-28 22:20:18 +0530

    some fixes to course page

--- app/src/pages/Courses/Courses.jsx
+++ app/src/pages/Courses/Courses.jsx
@@ -1,124 +1,114 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import styles from "./style.module.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Table from "../../components/Table/Table";
 
+import cx from 'classnames';
+
 export default function Courses() {
+
+  const [ug, setUg] = useState(false);
+
   return (
     <Fragment>
       <Header sticky />
       <main className={styles.main}>
-        <Table data={data} />
+        <div className={styles.btn__container}>
+          <button onClick={() => setUg(true)} className={ug ? cx(styles.btn1, styles.active) : styles.btn1}>UG Courses</button>
+          <button onClick={() => setUg(false)} className={ug ? styles.btn2 : cx(styles.btn2, styles.active)}>PG Courses</button>
+        </div>
+        {ug ?
+          <div style={{ marginTop: "5rem" }}>
+            <h4 className={styles.hero__sub_heading}>UG Courses</h4>
+            <Table data={ugCourses} />
+          </div> :
+          <div style={{ marginTop: "5rem" }}>
+            <h4 className={styles.hero__sub_heading}>PG Courses</h4>
+            <Table data={pgCourses} />
+          </div>}
       </main>
       <Footer />
     </Fragment>
   );
 }
 
-const data = {
+const ugCourses = {
   header: ["Department", "Course"],
   values: [
-    ["Automobile", "B.E. Automobile Engineering"],
-    [
-      <p>
-        Mr.R.Balasubramania
-        <br /> Former Member,
-        <br /> Tamilnadu Electricity Regulatory Commission,
-        <br /> Chennai, & Chief Engineer,
-        <br /> TNEB, Chennai
-      </p>,
-      <p>
-        Former Member Secretary, TERC,
+    ["Department of Automobile Engineering",
+      <>
+        <p>B.E. Automobile Engineering</p>
         <br />
-        258 / 6, Swathi Appartments,
+        <p>B.E. Mechanical Engineering</p>
         <br />
-        Opp.to Tirumangalam Police Station
-        <br /> Chennai - 600 101.
-      </p>,
-      "Chairman",
-      "seethalakshmib@hotmail.com",
-      "9840055645",
+        <p>B.E. Mechanical Engineering(Tamil Medium)</p>
+      </>
     ],
-    [
-      <p>
-        Mr.R.Balasubramania
-        <br /> Former Member,
-        <br /> Tamilnadu Electricity Regulatory Commission,
-        <br /> Chennai, & Chief Engineer,
-        <br /> TNEB, Chennai
-      </p>,
-      <p>
-        Former Member Secretary, TERC,
+    ["Department of Bio-Technology", "B.Tech Bio-Technology"],
+    ["Department of Civil Engineering",
+      <>
+        <p>B.E. Civil Engineering</p>
         <br />
-        258 / 6, Swathi Appartments,
-        <br />
-        Opp.to Tirumangalam Police Station
-        <br /> Chennai - 600 101.
-      </p>,
-      "Chairman",
-      "seethalakshmib@hotmail.com",
-      "9840055645",
+        <p>B.E. Civil Engineering(Tamil Medium)</p>
+      </>
     ],
-    [
-      <p>
-        Mr.R.Balasubramania
-        <br /> Former Member,
-        <br /> Tamilnadu Electricity Regulatory Commission,
-        <br /> Chennai, & Chief Engineer,
-        <br /> TNEB, Chennai
-      </p>,
-      <p>
-        Former Member Secretary, TERC,
+    ["Department of Electrical & Electronics Engineering", "B.E. Electrical & Electronics Engineering"],
+    ["Department of Electronics and Communication Engineering", "B.E. Electronics and Communication Engineering"],
+    ["Department of Information Technology",
+      <>
+        <p>B.Tech Information Technology</p>
         <br />
-        258 / 6, Swathi Appartments,
-        <br />
-        Opp.to Tirumangalam Police Station
-        <br /> Chennai - 600 101.
-      </p>,
-      "Chairman",
-      "seethalakshmib@hotmail.com",
-      "9840055645",
+        <p>B.E. Computer Science Engineering</p>
+      </>
     ],
-    [
-      <p>
-        Mr.R.Balasubramania
-        <br /> Former Member,
-        <br /> Tamilnadu Electricity Regulatory Commission,
-        <br /> Chennai, & Chief Engineer,
-        <br /> TNEB, Chennai
-      </p>,
-      <p>
-        Former Member Secretary, TERC,
+    ["Department of Petrochemical Technology", "B.Tech Petrochemical Technology"],
+    ["Department of Pharmaceutical Technology", "B.Tech Pharmaceutical Technology"]
+  ]
+};
+
+
+const pgCourses = {
+  header: ["Department", "Course"],
+  values: [
+    ["Department of Automobile Engineering",
+      <>
+        <p>M.E. Energy Engineering(Full Time)</p>
         <br />
-        258 / 6, Swathi Appartments,
+        <p>M.E. Manufacturing Engineering(Part Time)</p>
         <br />
-        Opp.to Tirumangalam Police Station
-        <br /> Chennai - 600 101.
-      </p>,
-      "Chairman",
-      "seethalakshmib@hotmail.com",
-      "9840055645",
+        <p>M.E. Thermal Engineering(Part Time)</p>
+      </>
     ],
-    [
-      <p>
-        Mr.R.Balasubramania
-        <br /> Former Member,
-        <br /> Tamilnadu Electricity Regulatory Commission,
-        <br /> Chennai, & Chief Engineer,
-        <br /> TNEB, Chennai
-      </p>,
-      <p>
-        Former Member Secretary, TERC,
+    ["Department of Bio-Technology", "M.Tech Bio-Technology(Full Time)"],
+    ["Department of Computer Applications",
+      <>
+        <p>MCA</p>
         <br />
-        258 / 6, Swathi Appartments,
-        <br />
-        Opp.to Tirumangalam Police Station
-        <br /> Chennai - 600 101.
-      </p>,
-      "Chairman",
-      "seethalakshmib@hotmail.com",
-      "9840055645",
+        <p>MCA Lateral Entry</p>
+      </>
     ],
-  ],
+    ["Department of Civil Engineering",
+      <>
+        <p>M.E. Environment Engineering(Full Time)</p>
+        <br />
+        <p>M.E. Structural Engineering(Part Time)</p>
+      </>
+    ],
+    ["Department of Electrical & Electronics Engineering", "M.E. Power System Engineering(Part Time)"],
+    ["Department of Electronics and Communication Engineering", "M.E. Communication Systems(Part Time)"],
+    ["Department of Information Technology",
+      <>
+        <p>M.E. Computer Science and Engineering(Full Time)</p>
+        <br />
+        <p>M.E. Computer Science and Engineering(Part Time)</p>
+        <br />
+        <p>M.E. Mobile & Pervasive Computing(Full Time)</p>
+        <br />
+        <p>M.E. Software Engineering(Full Time)</p>
+      </>
+    ],
+    ["Department of Management Studies", "M.B.A.(Full Time)"],
+    ["Department of Physics", "M.Tech Nano Science and Technology(Full Time)"]
+  ]
 };
