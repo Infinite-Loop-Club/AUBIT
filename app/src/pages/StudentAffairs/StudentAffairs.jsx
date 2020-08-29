53bd61d9016f4b402b899d88a02ba3728e15874a
Pranav <pranav.gnanasekar23@gmail.com>
2020-08-29 12:33:36 +0530

    student affairs start

--- app/src/pages/StudentAffairs/StudentAffairs.jsx
+++ app/src/pages/StudentAffairs/StudentAffairs.jsx
@@ -14,6 +14,45 @@ export default () => {
         >
           Student Affairs
         </h3>
+        <div className={styles.container}>
+          <p
+            className={styles.description}
+            style={{ marginBottom: "2rem", textIndent: "5rem" }}
+          >
+            Welcome to this page and thank you for taking a moment to read these
+            words introducing you to student affairs. Office of the Student
+            Affairs is a multi-faceted office that strives to provide students
+            with the necessary tools and support to make difficult situations
+            more manageable. The staffs in the Office of the Student Affairs
+            provide a student-centered approach to enhance the student
+            experience by servicing and maintain an inclusive, welcoming, and
+            safe campus community.
+          </p>
+          <h5>The main objectives are:</h5>
+          <p className={styles.description} style={{ textIndent: "5rem" }}>
+            <ul>
+              <li>
+                Helps in receiving the receipt of approval orders related with
+                Readmission, Transfer (from DOTE) through Center of student
+                affairs, Chennai.
+              </li>
+              <li>
+                Helps in receiving the approval orders for name change of
+                students and break of study based on medical certificate from
+                Center of Student affairs ,Chennai.
+              </li>
+              <li>
+                To solve the discrepancy that arises at the time of payment of
+                semester fees to AUKDC, Chennai.
+              </li>
+              <li>
+                To issue all kinds of Bonafide certificates, Transfer
+                certificates and course completion certificates .
+              </li>
+              <li>To avail Scholarships for all categories</li>
+            </ul>
+          </p>
+        </div>
       </main>
       <Footer />
     </Fragment>
