22176a8764d1cb36d42dc4fe96bcba0ee28fa620
Pranav <pranav.gnanasekar23@gmail.com>
2020-08-30 11:26:34 +0530

    auto staff img add

--- app/src/components/ImageGallery/ImageGallery.jsx
+++ app/src/components/ImageGallery/ImageGallery.jsx
@@ -1,28 +1,36 @@
-import React, { useState } from 'react';
-import classes from './ImageGallery.module.scss';
+import React, { useState } from "react";
+import classes from "./ImageGallery.module.scss";
 
-import Viewer from 'react-viewer';
-
-
+import Viewer from "react-viewer";
 
 export default function ImageGallery(props) {
   const [isVisible, setVisible] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
   function GalleryItem({ src, alt, index }) {
-    return <img
-      className={classes.gallery__image}
-      src={src}
-      alt={alt}
-      onClick={() => { setVisible(true); setActiveIndex(index); }}
-    />
+    return (
+      <img
+        className={classes.gallery__image}
+        src={src}
+        alt={alt}
+        onClick={() => {
+          setVisible(true);
+          setActiveIndex(index);
+        }}
+      />
+    );
   }
 
   return (
     <>
       <div className={classes.gallery__container}>
-        {
-          props.sources.map((source, index) => <GalleryItem key={index} src={source.src} alt={!!source.alt ? `slide-${index}` : source.alt} index={index} />)
-        }
+        {props.sources.map((source, index) => (
+          <GalleryItem
+            key={index}
+            src={source.src}
+            alt={!!source.alt ? `slide-${index}` : source.alt}
+            index={index}
+          />
+        ))}
       </div>
       <Viewer
         visible={isVisible}
@@ -34,5 +42,5 @@ export default function ImageGallery(props) {
         downloadable
       />
     </>
-  )
+  );
 }
