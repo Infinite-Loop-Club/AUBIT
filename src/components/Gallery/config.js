3703cbda9f7992f8d6c5d76af32e2649fe03d069
sank2000 <santhoshvelr@yahoo.com>
2020-08-31 20:14:14 +0530

    slides constant file added

---
+++ src/components/Gallery/config.js
@@ -0,0 +1,22 @@
+const settings = {
+  lazyload: true,
+  nav: false,
+  mouseDrag: true,
+  loop: true,
+  items: 1,
+  gutter: 10,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  container: "#responsive",
+  responsive: {
+    "600": {
+      items: 2,
+    },
+    "1000": {
+      items: 3
+    }
+  },
+  controls: false
+};
+
+export { settings };
